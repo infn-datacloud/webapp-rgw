5c65eed69f3bc26b29debc0d0f1b4e3f866ab903
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-04-05 17:00:06 +0200

    Add horizontal rule and set full width

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -13,8 +13,9 @@ export const Page = (props: Props) => {
       <div id="sidebar" className="w-64 left-0 top-0 h-screen fixed z-1 overflow-auto">
         <Drawer />
       </div>
-      <div className="ml-64 p-8">
+      <div className="ml-64 p-8 w-full">
         <Title>{props.title}</Title>
+        <hr className="h-px w-full my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
         {props.children}
       </div>
     </>
