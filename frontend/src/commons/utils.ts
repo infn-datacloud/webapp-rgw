887cec3f2182456036ced38c50d03148a51469d6
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-04-06 18:35:49 +0200

    Add parseReadWrite utilty function

--- frontend/src/commons/utils.ts
+++ frontend/src/commons/utils.ts
@@ -1,6 +1,13 @@
+import { RWAccess } from "../models/bucket";
+
 export const getHumanSize = (size: number) => {
   if (size < 1000) return `${size} B`;
   if (size < 1000000) return `${(size / 1000).toFixed(1)} kB`;
   if (size < 1000000000) return `${(size / 1000000).toFixed(1)} MB`;
   if (size < 1000000000000) return `${(size / 1000000000).toFixed(1)} GB`;
+}
+
+export const parseReadWriteAccess = (rwAccess: RWAccess) => {
+  return rwAccess.read && rwAccess.write ? "R/W" :
+    rwAccess.read ? "R" : rwAccess.write ? "R" : "Unknown";
 }
