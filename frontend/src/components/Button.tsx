e5a36f906d1b2741fe544d28922b516f4c948260
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-04-12 17:10:22 +0200

    Add Button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react"
+
+interface ButtonProps {
+  type?: "button" | "reset" | "submit"
+  title: string,
+  icon?: ReactNode
+}
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className="border rounded p-4 hover:bg-neutral-200"
+      type={props.type}
+    >
+      <div className="flex">
+        {props.icon ?
+          <div className="w-5">{props.icon}</div>
+          : null}
+        <div className="w-20">
+          {props.title}
+        </div>
+      </div>
+
+    </button>
+  )
+}
