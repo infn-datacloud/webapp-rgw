1fa402c8921daf66a10148bf6128560333f504ca
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-04-17 12:13:35 +0200

    Add INFN highlight color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        infn: "#4496b8"
+      }
+    },
     container: {
       center: true
     }
