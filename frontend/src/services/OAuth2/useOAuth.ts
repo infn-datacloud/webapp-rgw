175b21b80f8807ec9de43773d389143eedb8cfdc
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-04-21 12:07:59 +0200

    Fix multi-line lint warning

--- frontend/src/services/OAuth2/useOAuth.ts
+++ frontend/src/services/OAuth2/useOAuth.ts
@@ -4,8 +4,10 @@ import { useContext } from "react";
 export const useOAuth = (): OAuthContextProps => {
   const context = useContext(OAuthContext);
   if (!context) {
-    throw new Error("OAuthProvider context is undefined, please verify you are \
-    calling useOAuth() as child of a <OAuthProvider> componet.");
+    throw new Error(
+      "OAuthProvider context is undefined, please verify you are " +
+      "calling useOAuth() as child of a <OAuthProvider> componet."
+    );
   }
   return context;
 }
