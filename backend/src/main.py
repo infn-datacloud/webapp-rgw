c85ccb85465c7ceda35c300917302882d4fd0d14
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-05-02 18:48:36 +0200

    feat(api): move / to /api/v1/

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from routes import buckets, oauth
 
 app = FastAPI()
+v1 = FastAPI()
 
-app.include_router(buckets.router)
-app.include_router(oauth.router)
+v1.include_router(buckets.router)
+v1.include_router(oauth.router)
+
+app.mount("/api/v1", v1)
