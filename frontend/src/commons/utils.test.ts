0884e2d3e62da9c38ba939acb006c0e02cf7e16d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-05-05 16:30:09 +0200

    feat(frontend): add Path class and relative tests

---
+++ frontend/src/commons/utils.test.ts
@@ -0,0 +1,205 @@
+import { Path } from "./utils";
+
+test("Path 1", () => {
+  const p = new Path("/this/is/a/path/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("/this/is/a/path");
+});
+
+test("Path 2", () => {
+  const p = new Path("/this/is/a/path");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("/this/is/a/path");
+});
+
+test("Path 3", () => {
+  const p = new Path("this/is/a/path/");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("this/is/a/path");
+});
+
+test("Path 4", () => {
+  const p = new Path("this/is/a/path");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("this/is/a/path");
+});
+
+test("Path 5", () => {
+  const p = new Path("/this/is/a/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("a");
+  expect(p.parent?.name).toBe("is");
+  expect(p.path).toBe("/this/is/a");
+});
+
+test("Path 6", () => {
+  const p = new Path("/this/is/a");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("a");
+  expect(p.parent?.name).toBe("is");
+  expect(p.path).toBe("/this/is/a");
+});
+
+test("Path 7", () => {
+  const p = new Path("this/is/a");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("a");
+  expect(p.parent?.name).toBe("is");
+  expect(p.path).toBe("this/is/a");
+});
+
+test("Path 8", () => {
+  const p = new Path("this/is/a/");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("a");
+  expect(p.parent?.name).toBe("is");
+  expect(p.path).toBe("this/is/a");
+});
+
+test("Path 9", () => {
+  const p = new Path("/this/is");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("is");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("/this/is");
+});
+
+test("Path 10", () => {
+  const p = new Path("/this/is/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("is");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("/this/is");
+});
+
+test("Path 11", () => {
+  const p = new Path("this/is");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("is");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("this/is");
+});
+
+test("Path 12", () => {
+  const p = new Path("this/is/");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("is");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("this/is");
+});
+
+test("Path 13", () => {
+  const p = new Path("/this");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("this");
+  expect(p.parent?.name).toBe("/");
+  expect(p.path).toBe("/this");
+});
+
+test("Path 14", () => {
+  const p = new Path("/this/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("this");
+  expect(p.parent?.name).toBe("/");
+  expect(p.path).toBe("/this");
+});
+
+test("Path 15", () => {
+  const p = new Path("this");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("this");
+  expect(p.parent?.name).toBe(undefined);
+  expect(p.path).toBe("this");
+});
+
+test("Path 16", () => {
+  const p = new Path("this/");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("this");
+  expect(p.parent?.name).toBe(undefined);
+  expect(p.path).toBe("this");
+});
+
+test("Path 17", () => {
+  const p = new Path("/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("/");
+  expect(p.parent?.name).toBe("/");
+  expect(p.path).toBe("/");
+});
+
+test("Path 18", () => {
+  const p = new Path("");
+  expect(p.isAbsolute).toBe(false);
+  expect(p.name).toBe("");
+  expect(p.parent?.name).toBe(undefined);
+  expect(p.path).toBe("");
+});
+
+test("Path 19", () => {
+  const p = new Path("/this/path//");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("/this/path");
+});
+
+test("Path 20", () => {
+  const p = new Path("/this/path///");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("this");
+  expect(p.path).toBe("/this/path");
+});
+
+test("Prefix 1", () => {
+  const p = new Path("/this/is/a/path/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("/this/is/a/path");
+  expect(p.removePrefix("/this/").path).toBe("is/a/path");
+});
+
+test("Prefix 2", () => {
+  const p = new Path("/this/is/a/path/");
+  expect(p.isAbsolute).toBe(true);
+  expect(p.name).toBe("path");
+  expect(p.parent?.name).toBe("a");
+  expect(p.path).toBe("/this/is/a/path");
+  expect(p.removePrefix("/this").path).toBe("is/a/path");
+});
+
+test("Prefix 3", () => {
+  const p = new Path("/this/is/a/path/");
+  expect(() => {
+    p.removePrefix("/is/a");
+  }).toThrow();
+});
+
+test("Prefix 4", () => {
+  const p = new Path("/this/is/a/path/");
+  expect(p.removePrefix("/this/is").path).toBe("a/path");
+});
+
+test("Concat 1", () => {
+  const p1 = new Path("/this/is/a/path/");
+  const p2 = new Path("foo");
+  const p3 = p1.concat(p2);
+  expect(p3.path).toBe("/this/is/a/path/foo");
+})
+
+test("Concat 2", () => {
+  const p1 = new Path("/this/is/a/path");
+  const p2 = new Path("/foo");
+  const p3 = p1.concat(p2);
+  expect(p3.path).toBe("/this/is/a/path/foo");
+})
--- frontend/src/commons/utils.ts
+++ frontend/src/commons/utils.ts
@@ -11,3 +11,73 @@ export const parseReadWriteAccess = (rwAccess: RWAccess) => {
   return rwAccess.read && rwAccess.write ? "R/W" :
     rwAccess.read ? "R" : rwAccess.write ? "R" : "Unknown";
 }
+
+interface PathI {
+  path: string,
+  parent?: Path;
+  name: string
+  isAbsolute: boolean;
+  removePrefix: (prefix: string) => Path;
+  concat: (_: Path) => Path;
+}
+
+export class Path implements PathI {
+  rawPath: string;
+  pathElements: string[];
+
+  constructor(path: string) {
+    this.rawPath = path === "/" ? path : path.replace(/(\/+)$/, "");  // Remove trailing slashes
+    this.pathElements = this.rawPath.split('/');
+  }
+
+  get path() {
+    return this.rawPath;
+  }
+
+  removePrefix(prefix: string) {
+    if (!this.rawPath.startsWith(prefix)) {
+      throw new Error(`Prefix ${prefix} not found in path ${this.rawPath}`);
+    }
+    prefix = prefix === "/" ? prefix : prefix.replace(/(\/+)$/, "/");
+    prefix = !prefix.endsWith("/") ? prefix + '/' : prefix;
+    const re = new RegExp(`^${prefix}`);
+    const newPath = this.path.replace(re, "");
+    return new Path(newPath);
+  }
+
+  get name() {
+    if (this.path === '/') {
+      return '/';
+    }
+    return this.pathElements[this.pathElements.length - 1];
+  }
+
+  get parent(): Path | undefined {
+    if (this.path === '/') {
+      return new Path('/');
+    }
+    switch (this.pathElements.length) {
+      case 0:
+        throw new Error("Invalid path lenght: 0");
+      case 1:
+        return this.isAbsolute ? new Path("/") : undefined;
+      case 2:
+        return new Path(this.isAbsolute ? "/" : this.pathElements[0]);
+      default:
+        let newPathElements = [...this.pathElements];
+        newPathElements.pop();
+        const newPathString = newPathElements.join("/")
+        return new Path(newPathString);
+    }
+  }
+
+  get isAbsolute() {
+    return this.path.startsWith("/");
+  }
+
+  concat(newPath: Path) {
+    return newPath.isAbsolute ?
+      new Path(this.rawPath + newPath.path) :
+      new Path(this.rawPath + '/' + newPath.path);
+  }
+}
