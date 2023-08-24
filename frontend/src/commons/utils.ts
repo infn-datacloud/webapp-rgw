41bc090170893608e318a8a3ed5e51b21ff5b798
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-08-24 12:12:47 +0200

    fix(frontend): getHumanSize now returns a defined string

--- frontend/src/commons/utils.ts
+++ frontend/src/commons/utils.ts
@@ -5,6 +5,7 @@ export const getHumanSize = (size: number) => {
   if (size < 1000000) return `${(size / 1000).toFixed(1)} kB`;
   if (size < 1000000000) return `${(size / 1000000).toFixed(1)} MB`;
   if (size < 1000000000000) return `${(size / 1000000000).toFixed(1)} GB`;
+  return "N/A";
 }
 
 export const parseReadWriteAccess = (rwAccess: RWAccess) => {
