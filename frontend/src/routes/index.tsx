e0bab94da56bd7d6fc0c24302bb3b682ad963607
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-10-23 23:31:36 +0200

    fix(frontend): Delete unused OAuth2 service

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import { Home } from './Home';
 import { BucketAdministration } from './BucketAdministration';
-import { OAuthPopup } from '../services/OAuth2';
 import { Login } from './Login';
 
 export type Route = {
@@ -14,6 +13,5 @@ export type Route = {
 export const staticRoutes: Route[] = [
   { title: "Home", path: "/", element: <Home />, drawer: true },
   { title: "Buckets", path: "/buckets", element: <BucketAdministration />, drawer: true },
-  { title: "OAuth2Callback", path: "/callback", element: <OAuthPopup />, drawer: false },
   { title: "Login", path: "/login", element: <Login />, drawer: false }
 ];
--- frontend/src/services/OAuth2/index.ts
+++
@@ -1,6 +0,0 @@
-import { OAuthProviderProps } from "./OidcConfig";
-export * from "./OAuthProvider";
-export * from "./OAuthPopup";
-export * from "./OAuthContext";
-export * from "./useOAuth";
-export type { OAuthProviderProps }
