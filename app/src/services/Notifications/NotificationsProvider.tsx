0f42f123bb75fc6d5242e3c68de548f001bf8efa
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-11-30 15:37:13 +0100

    feat: Improve NotificationProvider with reducer

--- app/src/services/Notifications/NotificationsProvider.tsx
+++ app/src/services/Notifications/NotificationsProvider.tsx
@@ -1,45 +1,43 @@
-import { useRef, useState } from "react";
-import { NotificationProps, NotificationType } from "./types";
+import { useReducer } from "react";
+import { NotificationType } from "./types";
 import { NotificationsContext } from "./NotificationsContext";
+import { initialState, reducer } from "./reducer";
 
 interface NotificationsProviderBaseProps {
-	children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export interface NotificationsProviderProps extends NotificationsProviderBaseProps { }
 
 export const NotificationsProvider = (props: NotificationsProviderProps): JSX.Element => {
-	const { children } = props;
-	const [notifications, setNotifications] = useState<NotificationProps[]>([]);
-	const latestNotifications = useRef(notifications);
+  const { children } = props;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { notifications } = state;
 
-	function handleDelete(id: number) {
-		latestNotifications.current = [...latestNotifications.current].filter(n => n.id !== id);
-		setNotifications(latestNotifications.current);
-	}
+  function handleDelete(id: number) {
+    dispatch({ notifications: notifications.filter(n => n.id !== id) });
+  }
 
-	function notify(title: string, subtitle?: string, type: NotificationType = NotificationType.info, timeout: number = 5000) {
-		const notification = {
-			id: Date.now(),
-			title: title,
-			subtitle: subtitle,
-			timeout: timeout,
-			type: type,
-			onDelete: handleDelete
-		};
-
-		latestNotifications.current = [...notifications, notification]
-		setNotifications(latestNotifications.current);
-		console.log("Notification", title, subtitle);
-	}
-	return (
-		<NotificationsContext.Provider
-			value={{
-				notify,
-				notifications
-			}}
-		>
-			{children}
-		</NotificationsContext.Provider>
-	)
+  function notify(title: string, subtitle?: string, type: NotificationType = NotificationType.info, timeout: number = 5000) {
+    const notification = {
+      id: Date.now(),
+      title: title,
+      subtitle: subtitle,
+      timeout: timeout,
+      type: type,
+      onDelete: handleDelete
+    };
+    dispatch({ notifications: notifications.concat(notification) });
+    console.log("Notification", title, subtitle);
+  }
+  return (
+    <NotificationsContext.Provider
+      value={{
+        notify,
+        notifications
+      }}
+    >
+      {children}
+    </NotificationsContext.Provider>
+  )
 }
