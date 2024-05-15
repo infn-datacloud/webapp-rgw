c57f6d015bb5806e466a69f57c96d26d3fe7a6ba
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-15 10:27:09 +0000

    Remove custom env scripts

--- app/public/env.template.js
+++
@@ -1,9 +0,0 @@
-// Do not change this file. This is used to parse a real env.js file by
-// frontend/init_env.sh
-
-window.env = {
-  S3_ENDPOINT: "S3_ENDPOINT_VALUE",
-  S3_REGION: "S3_REGION_VALUE",
-  S3_ROLE_ARN: "S3_ROLE_ARN_VALUE",
-  S3_ROLE_DURATION_SECONDS: "S3_ROLE_DURATION_SECONDS_VALUE",
-};
