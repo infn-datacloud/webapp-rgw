d1402a87593e46d611a979001b388439e9203001
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-28 17:43:05 +0000

    Inherit ToggleSwitchProps from HTMLInputElement

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -1,28 +1,29 @@
-export interface ToggleSwitchProps {
-  checked: boolean;
-  onClick: () => void;
+export interface ToggleSwitchProps extends React.HTMLProps<HTMLInputElement> {
+  defaultChecked?: boolean;
 }
 
-export const ToggleSwitch = ({ checked, onClick }: ToggleSwitchProps) => {
+export default function ToggleSwitch(props: ToggleSwitchProps) {
+  let { type, className, defaultChecked, ...other } = props;
+  type = "checkbox";
+  className = "sr-only peer";
+
   return (
     <label className="relative inline-flex items-center cursor-pointer">
       <input
-        type="checkbox"
-        value=""
-        className="sr-only peer"
-        checked={checked}
-        onChange={onClick}
-        onClick={onClick}
+        type={type}
+        className={className}
+        defaultChecked={defaultChecked}
+        {...other}
       />
       <div
-        className="w-11 h-6 bg-gray-200 peer-focus:outline-none \
-          peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer \
-          peer-checked:after:translate-x-full peer-checked:after:border-white \
-          after:content-[''] after:absolute after:top-[2px] after:left-[2px] \
-          after:bg-white after:border-gray-300 after:border \
-          after:rounded-full after:h-5 after:w-5 after:transition-all \
+        className="w-11 h-6 bg-gray-200 peer-focus:outline-none 
+          peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer 
+          peer-checked:after:translate-x-full peer-checked:after:border-white 
+          after:content-[''] after:absolute after:top-[2px] after:left-[2px] 
+          after:bg-white after:border-gray-300 after:border 
+          after:rounded-full after:h-5 after:w-5 after:transition-all 
           peer-checked:bg-blue-600"
       ></div>
     </label>
   );
-};
+}
