8c655c644308c6901fa948422cecf0add664ce6a
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-28 18:02:41 +0000

    Make use of makeS3Client

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,28 +1,11 @@
 import { Page } from "@/components/Page";
-import { BucketInfo } from "@/models/bucket";
-import { auth } from "@/auth";
-import { S3Service } from "@/services/s3";
-import { s3ClientConfig } from "@/services/s3/actions";
+import { makeS3Client } from "@/services/s3/actions";
 import { makeTableData } from "./utils";
 import BucketsTable from "./buckets-table";
 
 export default async function Home() {
-  const session = await auth();
-  let bucketsInfos: BucketInfo[] = [];
-
-  if (!session) {
-    throw Error("Session is not available");
-  }
-
-  const { credentials } = session;
-  if (credentials) {
-    const s3Config = await s3ClientConfig(credentials);
-    const s3 = new S3Service(s3Config);
-    bucketsInfos = await s3.getBucketsInfos();
-  } else {
-    throw new Error("Cannot find credentials");
-  }
-
+  const s3 = await makeS3Client();
+  const bucketsInfos = await s3.getBucketsInfos();
   const tableData = makeTableData(bucketsInfos);
 
   return (
