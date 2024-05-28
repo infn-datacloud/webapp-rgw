3cf0bcb49316ebb728731c0788aa221482a9a419
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-28 17:42:37 +0000

    Add CloseButton component

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,16 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+export default function CloseButton(props: { onClose: () => void }) {
+  const { onClose } = props;
+  return (
+    <button onClick={onClose}>
+      <div
+        className="w-6 p-[3px] bg-neutral-300 text-neutral-500
+                   hover:bg-neutral-400 rounded-full"
+        aria-label="close"
+      >
+        <XMarkIcon />
+      </div>
+    </button>
+  );
+}
