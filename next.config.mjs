d010ff879f2ebd34843c00770055456c21cce62e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-28 09:33:44 +0000

    Redirect "/" to "/home"

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
