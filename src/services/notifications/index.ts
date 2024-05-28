871270c17d0283fc81e7edb4a286702180aad99c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-05-28 18:54:16 +0000

    Delete unused files

--- src/services/notifications/index.ts
+++
@@ -1,6 +0,0 @@
-import { Notification } from "./component";
-import { NotificationType } from "./types";
-import { useNotifications } from "./useNotifications";
-import { withNotifications } from "./withNotifications";
-
-export { Notification, NotificationType, useNotifications, withNotifications };
