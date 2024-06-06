297c482d3860439f1b1d469663b8dcbd83be2d3e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-06 16:41:31 +0200

    feat: Change color to ToggleSwitch

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -17,12 +17,12 @@ export default function ToggleSwitch(props: ToggleSwitchProps) {
       />
       <div
         className="w-11 h-6 bg-gray-200 peer-focus:outline-none 
-          peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer 
+          peer-focus:ring-4 peer-focus:ring-primary-light rounded-full peer 
           peer-checked:after:translate-x-full peer-checked:after:border-white 
           after:content-[''] after:absolute after:top-[2px] after:left-[2px] 
           after:bg-white after:border-gray-300 after:border 
           after:rounded-full after:h-5 after:w-5 after:transition-all 
-          peer-checked:bg-blue-600"
+          peer-checked:bg-primary"
       ></div>
     </label>
   );
