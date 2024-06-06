336fb3e6da766f29f1b411a424b2d3379c8e2ef1
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-06 15:34:37 +0200

    feat: Add missing colors to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,30 +19,53 @@ export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
 }
 
 function toColorClasses(colorType: ButtonColor) {
+  let className = "primary";
   switch (colorType) {
     case "primary":
-      return "bg-primary text-secondary";
+      className = "bg-primary hover:bg-primary-hover text-secondary";
+      break;
     case "primary-outline":
-      return "secondary border-color-primary"
+      className =
+        "bg-secondary border border-color-primary text-primary " +
+        "hover:bg-primary-hover";
+      break;
+    case "secondary":
+      className = "bg-secondary hover:bg-secondary-hover text-primary";
+      break;
+    case "secondary-outline":
+      className = "bg-primary border border-color-secondary text-secondary";
+      break;
+    case "warning":
+      className = "bg-warning hover:bg-warning-hover text-primary";
+    case "warning-outline":
+      className =
+        "bg-secondary border border-color-warning text-warning " +
+        "hover:bg-warning-hover";
+    case "danger":
+      className = "bg-danger text-secondary hover:bg-danger-hover";
+      break;
+    case "danger-outline":
+      className =
+        "bg-secondary border border-color-danger text-danger" +
+        "hover:bg-danger-hover";
     default:
-      return "primary";
   }
+  return className;
 }
 
 export const Button = (props: ButtonProps) => {
   const { className, icon, title, color, ...other } = props;
   const colorClasses = toColorClasses(color ?? "primary");
 
-  const buttonClasses =
-    `w-full border border-color rounded-full p-2 ${colorClasses}`
-    // TODO: `${other.disabled ? "text-neutral-200" : "hover:bg-neutral-200"}`;
+  const buttonClasses = `w-full rounded-full p-2 ${colorClasses}`;
+  // TODO: `${other.disabled ? "text-neutral-200" : "hover:bg-neutral-200"}`;
 
   return (
     <div className={className}>
       <button className={buttonClasses} {...other}>
         <div className="flex justify-center w-full">
-          {icon ? <div className="w-5 mr-4">{icon}</div> : null}
-          <div>{title}</div>
+          {icon ? <div className="w-5 mr-2 my-auto">{icon}</div> : null}
+          <small>{title}</small>
         </div>
       </button>
     </div>
