8ef6fd71a1e809490ad714266b283bdb061fded2
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-06 15:24:01 +0200

    fix: Remove white text in dark mode. Will include this in future

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const Title = ({ children }: Props) => {
   return (
-    <div className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+    <div className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
       {children}
     </div>
   );
