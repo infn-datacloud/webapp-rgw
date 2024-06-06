f610e90f07fca3d03a0391f108ba07d62d9db56f
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-06 16:38:04 +0200

    fix: Extend tailwind colors instead of overwriting them

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,37 +2,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      primary: {
-        DEFAULT: "#002a48",
-        50: "#002a4880",
-        hover: "#20385a",
-        light: "#4196b4",
-        hover: "#347890",
-      },
-      secondary: {
-        DEFAULT: "#fefefe",
-        50: "#fefefe80",
-        hover: "#e2e2e2",
-      },
-      success: {
-        DEFAULT: "#3da657",
-        50: "#35de2380",
-        hover: "#3da65760",
-      },
-      warning: {
-        DEFAULT: "#ffc107",
-        50: "#ffc10780",
-        hover: "#ffc822",
-      },
-      danger: {
-        DEFAULT: "#de2335",
-        50: "#de233580",
-        hover: "#ed2639",
-      },
-    },
     extend: {
       container: {
         center: true,
@@ -43,6 +12,37 @@ module.exports = {
       },
       textColor: {
         DEFAULT: "#dc2626",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: {
+          DEFAULT: "#002a48",
+          50: "#002a4880",
+          hover: "#20385a",
+          light: "#4196b4",
+          hover: "#347890",
+        },
+        secondary: {
+          DEFAULT: "#fefefe",
+          50: "#fefefe80",
+          hover: "#e2e2e2",
+        },
+        success: {
+          DEFAULT: "#3da657",
+          50: "#35de2380",
+          hover: "#3da65760",
+        },
+        warning: {
+          DEFAULT: "#ffc107",
+          50: "#ffc10780",
+          hover: "#ffc822",
+        },
+        danger: {
+          DEFAULT: "#de2335",
+          50: "#de233580",
+          hover: "#ed2639",
+        },
       },
     },
     plugins: [],
