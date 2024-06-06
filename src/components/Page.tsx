b453d94135475c6a427c65059521e4a39e516088
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-06 16:22:48 +0200

    feat: Page title to same component and use colors

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,3 @@
-import { Title } from "./Title";
-
 type Props = {
   title: string;
   children: React.ReactNode;
@@ -9,7 +7,7 @@ export const Page = (props: Props) => {
   const { title, children } = props;
   return (
     <>
-      <Title>{title}</Title>
+      <h1 className="text-primary">{title}</h1>
       <hr className="h-px w-full my-8 bg-gray-200 border-0"></hr>
       <div className="container px-16">{children}</div>
     </>
