9e10d512156ae8789b08d85afa263bae94a1b7ba
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-11 11:37:15 +0200

    feat: Restyle Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,23 +2,15 @@ export interface InputProps extends React.HTMLProps<HTMLInputElement> {
   error?: string;
 }
 
-export default function Input(props: InputProps) {
-  const { className, error, ...others } = props;
-
-  const inputClassName =
-    `placeholder:${error ? "text-danger" : "text-primary-light"} ` +
-    `block bg-secondary w-full border ${
-      error ? "border-danger" : "border-primary"
-    } ` +
-    `rounded-full py-1 pl-4 pr-3 shadow-sm focus:outline-none ` +
-    `focus:${error ? "border-danger" : "border-primary"}` +
-    `focus:${error ? "ring-red-danger" : "ring-primary"}` +
-    `focus: ring-1 smbre: text - sm`;
+export default function Input(props: Readonly<InputProps>) {
+  const { error, ...others } = props;
+  const className =
+    "placeholder:text-secondary-light rounded-full bg-secondary border py-2 px-3 border-primary text-primary";
 
   return (
-    <div className={className}>
-      <input className={inputClassName} {...others} />
-      {error ? <div className="text-red-400">{error}</div> : null}
+    <div className={props.className}>
+      <input className={className} {...others} />
+      {error ? <div className="text-danger">{error}</div> : null}
     </div>
   );
 }
