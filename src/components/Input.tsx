52766836e0499166caf0297d41fce2846c91781d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-11 11:57:26 +0200

    fix: Input not taking full width

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,13 +4,13 @@ export interface InputProps extends React.HTMLProps<HTMLInputElement> {
 
 export default function Input(props: Readonly<InputProps>) {
   const { error, ...others } = props;
-  const className =
-    "placeholder:text-secondary-light rounded-full bg-secondary border py-2 px-3 border-primary text-primary";
-
   return (
     <div className={props.className}>
-      <input className={className} {...others} />
-      {error ? <div className="text-danger">{error}</div> : null}
+      <input
+        className="placeholder:text-secondary-light font-small w-full rounded-full border border-primary bg-secondary px-3 py-2"
+        {...others}
+      />
+      {error ? <div className="text-red-400">{error}</div> : null}
     </div>
   );
 }
