71c0c74348e53af8591d0bb8ae27fee1e968b8e7
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-11 11:24:01 +0200

    feat: Make login page responsive and add Ceph logo

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
-import logo from "@/imgs/infn.png";
+import logo from "@/imgs/logo-ceph.png";
 import LoginForm from "./components/login-form";
 import { Suspense } from "react";
 
 export default function Login() {
   return (
-    <div className="bg-secondary w-2/3 xl:w-1/2 max-w-3xl m-auto p-8 shadow-lg mt-16 rounded-lg">
+    <div className="m-auto mt-16 w-2/3 min-w-48 max-w-3xl rounded-lg bg-secondary p-8 md:w-1/2">
+      <h1 className="mx-auto text-center text-primary">
+        INFN Cloud Object Storage
+      </h1>
       <Image
         src={logo}
         className="mx-auto p-4"
         alt="INFN Cloud"
-        height={200}
+        height={100}
         priority={true}
       />
-      <h1 className="mx-auto text-center text-primary">Welcome</h1>
-      <div className="w-1/3 mx-auto mt-8 flex flex-col space-y-2">
+      <h2 className="mx-auto text-center text-primary">Welcome</h2>
+      <div className="mx-auto mt-8 flex flex-col space-y-2 min-w-full md:w-1/2 sm:min-w-0">
         <Suspense>
           <LoginForm />
         </Suspense>
