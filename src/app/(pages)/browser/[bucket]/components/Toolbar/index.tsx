1ecceb28af133de7f9c1a674b48d6ee037d17c52
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-13 11:02:01 +0200

    refactor: mark props as readonly

--- src/app/(pages)/browser/[bucket]/components/Toolbar/index.tsx
+++ src/app/(pages)/browser/[bucket]/components/Toolbar/index.tsx
@@ -5,17 +5,19 @@ import NewPathButton from "./NewPathButton";
 import DeleteButton from "./DeleteButton";
 import { _Object } from "@aws-sdk/client-s3";
 
-export default function Toolbar(props: {
-  bucket: string;
-  currentPath: string;
-  objectsToDelete: string[];
-  onPathChange?: (newPath: string) => void;
-  onDeleted?: () => void;
-}) {
+export default function Toolbar(
+  props: Readonly<{
+    bucket: string;
+    currentPath: string;
+    objectsToDelete: string[];
+    onPathChange?: (newPath: string) => void;
+    onDeleted?: () => void;
+  }>
+) {
   const { bucket, currentPath, objectsToDelete, onPathChange, onDeleted } =
     props;
   return (
-    <div className="flex mt-8 place-content-between">
+    <div className="mt-8 flex place-content-between">
       <div className="flex space-x-4">
         <HomeButton />
         <UploadButton bucket={bucket} currentPath={currentPath} />
