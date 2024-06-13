157f8d0386f0d58d2ed505fa2a142e777062061e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-13 12:06:42 +0200

    fix: Polymorphic date importing utils from wrong place

--- src/components/IsomorphicDate.tsx
+++ src/components/IsomorphicDate.tsx
@@ -1,12 +1,12 @@
-import { dateToHuman } from "@/app/(pages)/home/utils";
+import { dateToHuman } from "@/commons/utils";
 import { useEffect, useState } from "react";
 
 /**
  * @description Render a date SSR / CSR
  */
-export default function IsomorphicDate(props: { date: Date }) {
+export default function IsomorphicDate(props: Readonly<{ date: Date }>) {
   const { date } = props;
-  const [_date, setDate] = useState("");
-  useEffect(() => setDate(dateToHuman(date)), [date]);
-  return <>date</>;
+  const [polymorphicDate, setPolymorphicDate] = useState("");
+  useEffect(() => setPolymorphicDate(dateToHuman(date)), [date]);
+  return { polymorphicDate };
 }
