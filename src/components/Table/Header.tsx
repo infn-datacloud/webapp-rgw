0c5a37ed3f7f479937a6722bc74c0229456023f3
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-13 16:26:41 +0200

    fix: Table Header text color

--- src/components/Table/Header.tsx
+++ src/components/Table/Header.tsx
@@ -13,7 +13,7 @@ export const Header = (props: HeaderProps) => {
         {selectable ? <th></th> : null}
         {columns.map(column => (
           <th
-            className="border-b pb-3 pt-0 text-left font-medium text-slate-800 first:pl-8 last:pr-8"
+            className="border-b pb-3 pt-0 text-left font-medium text-primary first:pl-8 last:pr-8"
             key={column.id}
           >
             {column.name}
