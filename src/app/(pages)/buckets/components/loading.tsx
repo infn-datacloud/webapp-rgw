23681dbaf29dcb3f5823cfbda711a1336c52a352
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-06-14 12:51:39 +0200

    refactor: mark props readonly

--- src/app/(pages)/buckets/components/loading.tsx
+++ src/app/(pages)/buckets/components/loading.tsx
@@ -1,7 +1,7 @@
 import { ChartPieIcon, ClockIcon, CubeIcon } from "@heroicons/react/24/outline";
 import Subview from "./Subview";
 
-export function SummaryLoading(props: { bucket?: string }) {
+export function SummaryLoading(props: Readonly<{ bucket?: string }>) {
   const { bucket } = props;
   return (
     <div className="dark:bg-primary-dark mt-4 rounded border bg-secondary p-2 text-primary dark:text-secondary">
