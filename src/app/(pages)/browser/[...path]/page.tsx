71efd5f1c34ce535dde63b73cf4f6382a0de121c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-04-30 12:15:35 +0200

    Remove suspension and loading bar

--- src/app/(pages)/browser/[...path]/page.tsx
+++ src/app/(pages)/browser/[...path]/page.tsx
@@ -1,17 +1,6 @@
 import { Page } from "@/components/page";
 import { _Object } from "@aws-sdk/client-s3";
-import { Suspense } from "react";
 import { Browser } from "./components";
-
-function LoadingBar() {
-  return (
-    <div className="w-full">
-      <div className="h-1.5 w-full overflow-hidden rounded-full bg-primary/10 dark:bg-secondary/10">
-        <div className="h-full w-full origin-left animate-progress rounded-full bg-primary dark:bg-primary-200" />
-      </div>
-    </div>
-  );
-}
 
 type BrowserProps = {
   params: Promise<{ path: [string] }>;
@@ -30,9 +19,7 @@ export default async function BucketBrowser(props: Readonly<BrowserProps>) {
 
   return (
     <Page title={path[0]}>
-      <Suspense fallback={<LoadingBar />}>
-        <Browser path={path} count={count} nextContinuationToken={next} />
-      </Suspense>
+      <Browser path={path} count={count} nextContinuationToken={next} />
     </Page>
   );
 }
