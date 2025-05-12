827f8c4a8bb056e48faebf4649a0979a4ecf014e
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-12 12:42:35 +0200

    Restyle paginator

--- src/components/paginator/index.tsx
+++ src/components/paginator/index.tsx
@@ -9,9 +9,6 @@ type PaginatorProps = {
 };
 
 export default function Paginator(props: Readonly<PaginatorProps>) {
-  const className =
-    "flex p-0.5 ml-0 bg-white text-primary/50 border border-primary/10 hover:bg-infn/10 dark:bg-secondary/50 dark:text-secondary dark:hover:text-primary dark:hover:bg-white/70";
-
   const { nextContinuationToken } = props;
 
   const router = useRouter();
@@ -42,11 +39,36 @@ export default function Paginator(props: Readonly<PaginatorProps>) {
 
   return (
     <div className="dark:text-secondary flex w-full items-center justify-between leading-tight text-gray-500">
-      <div className="flex items-center space-x-2">
-        <div>Show</div>
+      <ul className="flex">
+        <li className="text-primary dark:text-secondary bg-secondary dark:bg-secondary/10 dark:hover:bg-secondary/20 min-w-16 rounded-tl-lg rounded-bl-lg border border-neutral-200 hover:bg-neutral-200">
+          <button
+            title="Previous Page"
+            className="inline-flex p-1.5"
+            onClick={goBack}
+          >
+            <ChevronLeftIcon className="size-5 h-[1lh]" />
+            <span>Back</span>
+          </button>
+        </li>
+        <li
+          className="group text-primary dark:text-secondary bg-secondary dark:bg-secondary/10 data-[disabled=false]:dark:hover:bg-secondary/20 data-[disabled=true]:dark:text-secondary/30 min-w-16 justify-end rounded-tr-lg rounded-br-lg border border-neutral-200 data-[disabled=false]:hover:bg-neutral-200 data-[disabled=true]:text-slate-400"
+          data-disabled={!nextContinuationToken}
+        >
+          <Link
+            title="Next Page"
+            className="flex justify-end p-1.5 group-data-[disabled=true]:cursor-default"
+            href={nextPage}
+          >
+            <span>Next</span>
+            <ChevronRightIcon className="size-5 h-[1lh]" />
+          </Link>
+        </li>
+      </ul>
+      <div className="flex items-center space-x-2 text-slate-500">
+        <span>Show</span>
         <select
           value={itemsPerPage}
-          className="dark:text-secondary/50 block rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white/10"
+          className="dark:text-secondary/80 block rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white/10"
           onChange={e => onChangeItemsPerPage(parseInt(e.currentTarget.value))}
         >
           <option value="10">10</option>
@@ -55,19 +77,8 @@ export default function Paginator(props: Readonly<PaginatorProps>) {
           <option value="100">100</option>
           <option value="200">200</option>
         </select>
+        <span>items</span>
       </div>
-      <ul className="flex">
-        <li>
-          <button title="Previous Page" className={className} onClick={goBack}>
-            <ChevronLeftIcon className="m-auto w-5" />
-          </button>
-        </li>
-        <li>
-          <Link title="Next Page" className={className} href={nextPage}>
-            <ChevronRightIcon className="m-auto w-5" />
-          </Link>
-        </li>
-      </ul>
     </div>
   );
 }
