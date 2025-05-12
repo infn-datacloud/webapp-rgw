c4840b772463840d215a2bc45b5027365600ab14
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-12 12:42:28 +0200

    Fix hover bg

--- src/app/(pages)/browser/[...path]/components/browser/toolbar/home-button.tsx
+++ src/app/(pages)/browser/[...path]/components/browser/toolbar/home-button.tsx
@@ -5,7 +5,7 @@ export default function HomeButton() {
   return (
     <Link
       href="/"
-      className="border-primary bg-secondary text-primary hover:bg-primary-hover hover:text-secondary dark:border-secondary dark:text-secondary flex items-center gap-2 rounded-full border p-1 px-3 text-sm dark:bg-transparent"
+      className="border-primary bg-secondary text-primary hover:bg-primary-hover hover:text-secondary dark:border-secondary dark:text-secondary hover:bg-primary flex items-center gap-2 rounded-full border p-1 px-3 text-sm dark:bg-transparent"
     >
       <HomeIcon className="size-5" />
       Home
