0799f5469721823f83b34d22d688ba52eee6eeb0
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-13 15:20:16 +0200

    Adjust login responsive design

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -5,21 +5,21 @@ import { Suspense } from "react";
 
 export default function Login() {
   return (
-    <div className="p-16">
-    <div className="m-auto mt-16 w-11/12 max-w-2xl rounded-lg bg-secondary p-8 text-primary sm:w-1/2 dark:bg-slate-800 dark:text-secondary">
-      <h1 className="mx-auto text-center">INFN Cloud Object Storage</h1>
-      <Image
-        src={logo}
-        className="mx-auto p-4"
-        alt="INFN Cloud"
-        height={100}
-        priority={true}
-      />
-      <h2 className="mx-auto text-center">Welcome</h2>
-      <div className="mx-auto mt-8 flex max-w-54 flex-col space-y-2">
-        <Suspense>
-          <LoginForm />
-        </Suspense>
+    <div className="flex h-screen items-center p-4 xl:items-start">
+      <div className="bg-secondary text-primary dark:text-secondary mx-auto rounded-xl p-8 shadow-2xl sm:max-w-96 dark:bg-slate-800">
+        <h1 className="mx-auto text-center">INFN Cloud Object Storage</h1>
+        <Image
+          src={logo}
+          className="mx-auto p-4"
+          alt="INFN Cloud"
+          height={100}
+          priority={true}
+        />
+        <h2 className="mx-auto text-center">Welcome</h2>
+        <div className="mx-auto mt-8 flex max-w-54 flex-col space-y-2">
+          <Suspense>
+            <LoginForm />
+          </Suspense>
         </div>
       </div>
     </div>
