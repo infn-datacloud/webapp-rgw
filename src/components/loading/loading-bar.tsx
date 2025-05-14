33f21f5865929a477909deadd77e2c206569eddf
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-14 09:35:30 +0200

    Fix animation class

--- src/components/loading/loading-bar.tsx
+++ src/components/loading/loading-bar.tsx
@@ -6,10 +6,10 @@ export function LoadingBar(props: Readonly<LoadingBarProps>) {
   const { show } = props;
   return (
     <div
-      className="h-1.5 w-full overflow-hidden rounded-full bg-primary/10 data-[hidden=true]:hidden dark:bg-secondary/10"
+      className="bg-primary/10 dark:bg-secondary/10 h-1.5 w-full overflow-hidden rounded-full data-[hidden=true]:hidden"
       data-hidden={!show}
     >
-      <div className="h-full w-full origin-left animate-progress rounded-full bg-primary dark:bg-primary-200" />
+      <div className="animate-wiggle bg-primary dark:bg-primary-200 h-full w-full origin-left rounded-full" />
     </div>
   );
 }
