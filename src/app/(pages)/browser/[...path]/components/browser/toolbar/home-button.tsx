1822dcf547c12303e974b71edbdef853dc1b41c1
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-05-16 10:02:58 +0200

    Adjust buttons dark mode hover bg

--- src/app/(pages)/browser/[...path]/components/browser/toolbar/home-button.tsx
+++ src/app/(pages)/browser/[...path]/components/browser/toolbar/home-button.tsx
@@ -7,10 +7,7 @@ import { HomeIcon } from "@heroicons/react/24/outline";
 
 export default function HomeButton() {
   return (
-    <Link
-      href="/"
-      className="border-primary bg-secondary text-primary hover:bg-primary-hover hover:text-secondary dark:border-secondary dark:text-secondary hover:bg-primary flex items-center gap-2 rounded-full border p-1 px-3 text-sm dark:bg-transparent"
-    >
+    <Link href="/" className="btn-secondary">
       <HomeIcon className="size-5" />
       Home
     </Link>
