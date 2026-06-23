140444b126dc8134180bba38a103c00974f94f6c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-06-23 12:18:37 +0200

    Separate auth providers in different files

---
+++ src/auth/index.ts
@@ -0,0 +1,114 @@
+// SPDX-FileCopyrightText: 2025 Istituto Nazionale di Fisica Nucleare
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import { betterAuth, BetterAuthOptions } from "better-auth";
+import { APIError } from "better-auth/api";
+import { nextCookies } from "better-auth/next-js";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { settings } from "@/config";
+import { Database } from "better-sqlite3";
+import { getOAuth2Session, oAuth } from "./oidc";
+import { getCredentialsSession, plainCredentials } from "./plain-credentials";
+
+const {
+  WEBAPP_RGW_BASE_URL,
+  WEBAPP_RGW_AUTH_SECRET,
+  WEBAPP_RGW_S3_ROLE_DURATION_SECONDS,
+} = settings;
+
+// https://github.com/better-auth/better-auth/blob/dfeefc4f58ee24f1ee6cdbe22d5e720b2ba621b8/packages/better-auth/src/plugins/username/index.ts#L98
+
+export const authConfig = (db: Database) => {
+  return {
+    baseURL: `${WEBAPP_RGW_BASE_URL}/api/auth`,
+    secret: WEBAPP_RGW_AUTH_SECRET,
+    database: db,
+    session: {
+      // expire 1 seconds before aws credentials
+      expiresIn: WEBAPP_RGW_S3_ROLE_DURATION_SECONDS - 1,
+      disableSessionRefresh: true,
+      additionalFields: {
+        accessKeyId: {
+          type: "string",
+        },
+        secretAccessKey: {
+          type: "string",
+        },
+        sessionToken: {
+          type: "string",
+          defaultValue: null,
+        },
+        groups: {
+          type: "string[]",
+        },
+      },
+    },
+    databaseHooks: {
+      session: {
+        create: {
+          before: async (sessionData, ctx) => {
+            if (!ctx) {
+              throw new APIError("UNAUTHORIZED", {
+                message: "Context session not found",
+              });
+            }
+            if (ctx.path === "/oauth2/callback/:providerId") {
+              return getOAuth2Session(sessionData, ctx);
+            } else if (ctx.path === "/sign-in/credentials") {
+              return getCredentialsSession(sessionData, ctx);
+            }
+            return { data: { ...sessionData } };
+          },
+        },
+      },
+    },
+    account: {
+      updateAccountOnSignIn: true,
+    },
+    plugins: [oAuth(), plainCredentials(), nextCookies()],
+  } satisfies BetterAuthOptions;
+};
+
+export const auth = betterAuth(authConfig(globalThis.db));
+export type User = typeof auth.$Infer.Session.user;
+export type Session = typeof auth.$Infer.Session;
+
+export async function signInCredentials(
+  accessKeyId: string,
+  secretAccessKey: string
+) {
+  try {
+    await auth.api.signInCredentials({
+      body: { accessKeyId, secretAccessKey },
+    });
+    redirect("/");
+  } catch (err) {
+    if (err instanceof APIError) {
+      redirect(`/login?error=${err.status}`);
+    }
+    throw err;
+  }
+}
+
+export async function signIn() {
+  const { url } = await auth.api.signInWithOAuth2({
+    body: {
+      providerId: "indigo-iam",
+      callbackURL: WEBAPP_RGW_BASE_URL,
+    },
+  });
+  redirect(url);
+}
+
+export async function signOut() {
+  await auth.api.signOut({ headers: await headers() });
+}
+
+export async function getSession() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  return session;
+}
---
+++ src/auth/oidc.ts
@@ -0,0 +1,82 @@
+// SPDX-FileCopyrightText: 2026 Istituto Nazionale di Fisica Nucleare
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+import { decodeJwtPayload } from "@/commons/utils";
+import { settings } from "@/config";
+import { loginWithSTS } from "@/services/s3/actions";
+import { GenericEndpointContext } from "better-auth";
+import { genericOAuth } from "better-auth/plugins";
+
+const {
+  WEBAPP_RGW_OIDC_ISSUER,
+  WEBAPP_RGW_OIDC_CLIENT_ID,
+  WEBAPP_RGW_OIDC_CLIENT_SECRET,
+  WEBAPP_RGW_OIDC_SCOPE,
+  WEBAPP_RGW_OIDC_AUDIENCE,
+} = settings;
+
+export function oAuth() {
+  return genericOAuth({
+    config: [
+      {
+        providerId: "indigo-iam",
+        discoveryUrl: `${WEBAPP_RGW_OIDC_ISSUER}/.well-known/openid-configuration`,
+        clientId: WEBAPP_RGW_OIDC_CLIENT_ID,
+        clientSecret: WEBAPP_RGW_OIDC_CLIENT_SECRET,
+        scopes: WEBAPP_RGW_OIDC_SCOPE.split(" "),
+        pkce: true,
+        authorizationUrlParams: {
+          aud: WEBAPP_RGW_OIDC_AUDIENCE,
+        },
+      },
+    ],
+  });
+}
+
+type SessionData = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  expiresAt: Date;
+  token: string;
+  ipAddress?: string | null | undefined;
+  userAgent?: string | null | undefined;
+};
+
+export async function getOAuth2Session(
+  sessionData: SessionData,
+  ctx: GenericEndpointContext
+) {
+  const { userId } = sessionData;
+  // prettier-ignore
+  const [account] = await ctx.context
+                             .internalAdapter
+                             .findAccountByUserId(userId);
+  const { accessToken } = account;
+  if (!accessToken) {
+    throw new Error("cannot perform STS: access token not found");
+  }
+  const profile = decodeJwtPayload(accessToken);
+  const credentials = await loginWithSTS(accessToken);
+  // prettier-ignore
+  const {
+      accessKeyId,
+      secretAccessKey,
+      sessionToken,
+    } = credentials;
+  if (!(accessKeyId && secretAccessKey && sessionToken)) {
+    throw new Error("Failed to get AWS Credentials");
+  }
+  const groups = profile["groups"] ?? [];
+  return {
+    data: {
+      ...sessionData,
+      accessKeyId,
+      secretAccessKey,
+      sessionToken,
+      groups,
+    },
+  };
+}
