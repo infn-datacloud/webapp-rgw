28bee8808bf9996b396ac429d6e58e66bbf36383
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2026-07-01 11:17:29 +0200

    Show bucket name validation

--- src/app/buckets/components/toolbar/create-button/modal.tsx
+++ src/app/buckets/components/toolbar/create-button/modal.tsx
@@ -9,55 +9,39 @@ import Form from "@/components/form";
 import Modal, { ModalBody, ModalFooter } from "@/components/modal";
 import Input from "@/components/inputs/input";
 import { toaster } from "@/components/toaster";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { createBucket } from "./actions";
 
-const bucketValidator = new RegExp(
-  "(?!(^xn--|.+-s3alias$))^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$"
-);
-const InvalidBucketError = () => {
+function InvalidBucketError() {
   return (
-    <>
-      <p className="mt-1 text-xs font-bold">Bucket name is not valid.</p>
-      <p className="text-xs">Bucket name must:</p>
+    <div>
+      <p className="mt-1 text-xs font-bold">
+        Bucket name is not valid. It must:
+      </p>
       <ul className="ml-3 list-disc text-xs">
-        <li>be between 3(min) and 63(max) characters long.</li>
+        <li>be between 3 and 63 characters long</li>
         <li>
-          consist only of lowercase letters, numbers, dots(.), and hyphens(-).
+          consist only of lowercase letters, numbers,{" "}
+          <span className="font-code">.</span> and{" "}
+          <span className="font-code">-</span>
         </li>
-        <li>begin and end with a letter or number.</li>
-        <li>not contain two adjacent periods.</li>
-        <li>not be formatted as an IP address(for example, 192.168.5.4).</li>
-        <li>not start with the prefix xn--.</li>
-        <li>not end with the suffix - s3alias.</li>
-        <li>not end with the suffix--ol - s3.</li>
+        <li>begin and end with a letter or number</li>
+        <li>not contain two adjacent periods</li>
+        <li>
+          not be formatted as an IP address (i.e,{" "}
+          <span className="font-code">192.168.5.4</span>)
+        </li>
+        <li>
+          not start with the prefix <span className="font-code">xn--</span>
+        </li>
+        <li>
+          not end with the suffix <span className="font-code">-s3alias</span>
+        </li>
+        <li>
+          not end with the suffix <span className="font-code">--ol-s3</span>.
+        </li>
       </ul>
-    </>
-  );
-};
-
-function NewBucketNameInput() {
-  const [newBucket, setNewBucket] = useState("");
-  const [error, setError] = useState(false);
-
-  const validateBucketName = () => {
-    setError(bucketValidator.test(newBucket));
-  };
-
-  return (
-    <div className="mt-16 flex justify-between">
-      <div className="my-auto font-bold lg:w-52">Bucket Name*</div>
-      <div className="w-full">
-        <Input
-          name="new-bucket"
-          placeholder={"Enter a name for your bucket"}
-          value={newBucket}
-          onChange={e => setNewBucket(e.currentTarget.value)}
-          onBlur={() => validateBucketName()}
-        />
-      </div>
-      {/* {error ? <InvalidBucketError /> : null} */}
     </div>
   );
 }
@@ -85,7 +69,18 @@ type CreateBucketModal = {
 
 export default function CreateBucketModal(props: Readonly<CreateBucketModal>) {
   const { show, onClose } = props;
+  const [newBucket, setNewBucket] = useState("");
+  const [error, setError] = useState(false);
   const router = useRouter();
+  const reset = () => {
+    setNewBucket("");
+    setError(false);
+  };
+
+  const closeAndReset = () => {
+    reset();
+    onClose();
+  };
 
   const action = (formData: FormData) => {
     const submit = async () => {
@@ -93,7 +88,7 @@ export default function CreateBucketModal(props: Readonly<CreateBucketModal>) {
       if (!error) {
         toaster.success("Bucket successfully created");
         router.refresh();
-        onClose();
+        closeAndReset();
       } else {
         toaster.danger("Cannot create bucket", error);
       }
@@ -101,18 +96,45 @@ export default function CreateBucketModal(props: Readonly<CreateBucketModal>) {
     submit();
   };
 
+  const validateBucketName = useCallback(() => {
+    const pattern = /(?!(^xn--|.+-s3alias$))^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$/;
+    const isValid = pattern.test(newBucket);
+    setError(!isValid);
+  }, [newBucket]);
+
   return (
-    <Modal title="Create new bucket" show={show} onClose={onClose}>
+    <Modal title="Create new bucket" show={show} onClose={closeAndReset}>
       <Form action={action}>
         <ModalBody>
-          <NewBucketNameInput />
+          <div className="mt-16 flex items-start">
+            <div className="font-bold lg:w-52">Bucket Name*</div>
+            <div className="w-full">
+              <Input
+                name="new-bucket"
+                placeholder="Enter a name for your bucket"
+                onChange={e => setNewBucket(e.currentTarget.value)}
+                onBlur={validateBucketName}
+              />
+              {newBucket && error && <InvalidBucketError />}
+            </div>
+          </div>
           <BucketFeatures />
         </ModalBody>
         <ModalFooter>
-          <Button className="btn-tertiary" title="Clear" type="reset">
+          <Button
+            className="btn-tertiary"
+            title="Clear"
+            type="reset"
+            onClick={reset}
+          >
             Clear
           </Button>
-          <Button className="btn-primary" title="Create Bucket" type="submit">
+          <Button
+            className="btn-primary"
+            title="Create Bucket"
+            type="submit"
+            disabled={!newBucket || error}
+          >
             Create Bucket
           </Button>
         </ModalFooter>
